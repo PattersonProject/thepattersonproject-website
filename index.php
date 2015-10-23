25fd6dfe6bbb1d66eb2476aa5a2206058f1677b6
unknown <michael.pttrson@gmail.com>
2015-10-22 21:42:56 -0400

    Added a bit of css

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <!DOCTYPE htmp>
+<html lang="en">
 	<head>
 		<meta charset="UTF-8">
 		<title> Michael Patterson Project Manager </title>
-		<link rel="stylesheet" type="text/css" href="/css/index.css">
-		<!--<?php include("include/head.php");?> -->
+		<!-- <link rel="stylesheet" type="text/css" href="/css/index.css"> -->
+		<?php include("include/head.php");?> 
 	</head>
 
 
 	<body>
 
-		<?php include ("includes/header.php");?>
+
+		<?php include ("include/header.php");?>
 
 		<p>
 		I am a team focused Project Manager who reliably delivers results on time, and on budget.  After 15 years in the live entertainment industry I am putting my management skills to use in the tech world. My course work at Harvard Extension has provided me with a strong foundation in technology, and my years of professional experience give me a laser focus on stakeholder needs. 
 		</p> 
 		<p>
 		I am passionate about persuing my goals in my new field, and I never stop learning. I have a proven track record of facing new challanges, such as a degree in a whole new field, and quickly learning new skills. Check out my <a href="/content/Michael Patterson resume.pdf">resume</a> to read a bit more about my qualifications and 
+		</p>
 	</body>
+</html>
