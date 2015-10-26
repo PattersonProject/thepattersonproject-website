e6ae2e8a74fe286266b4ea0388dc5b85b198478a
unknown <michael.pttrson@gmail.com>
2015-10-26 09:58:50 -0400

    New header css, background image

--- include/head.php
+++ include/head.php
@@ -1,1 +1,3 @@
+	<link href='https://fonts.googleapis.com/css?family=Cantarell' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="css/pattersonproject.css">
 	<link rel="stylesheet" type="text/css" href="css/navigation.css">
--- include/header.php
+++ include/header.php
@@ -1,19 +1,19 @@
 <div id="header">
-<div id="nav">
-	<ul>
-		<li id="navacbout"><a href="index.php">About Me</a></li>
-		<li id="navresume"><a href="resume.php">My Resume</a></li>
-		<!-- <li id="navprojects"><a href="projects.php">My Projects</a></li> -->
-	</ul>
-</div>
+	<div id="nav">
+		<ul>
+			<li id="iamhere"><a href="index.php">About Me</a></li>
+			<li id="navresume"><a href="resume.php">My Resume</a></li>
+			<!-- <li id="navprojects"><a href="projects.php">My Projects</a></li> -->
+		</ul>
+	</div>
 
-<div id="logo">
-	<img src="images/logo.png" alt="Michael Patterson, Project Manager">
-</div>
+	<div id="logo">
+		<img src="images/logo.png" alt="Michael Patterson, Project Manager">
+	</div>
 
-<div id="menu">
-	<ul>
-		<li id="menuemail"><a href="email.php"><img src="images/email.png" alt="Contact me"></a></li>
-	</ul>
-</div>
+	<div id="menu">
+		<ul>
+			<li id="menuemail"><a href="email.php"><img src="images/email.png" alt="Email me"></a></li>
+		</ul>
+	</div>
 </div>
