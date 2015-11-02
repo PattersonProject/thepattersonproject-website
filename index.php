04472b28660971a5f111d8f33efb5b872052e51c
Michael Patterson <michael.pttrson@gmail.com>
2015-11-02 14:21:54 -0500

    Fixed commas and iamhere color

--- index.php
+++ index.php
@@ -16,11 +16,11 @@
 		<div id="content">
 			<img class="profile" src="images/profile-pic.png" alt="My profile picture">
 			<p>
-			I am a team focused Project Manager who reliably delivers results on time, and on budget.  After 15 years in the live entertainment industry I am putting my management skills to use in the tech world. My course work at Harvard Extension has provided me with a strong foundation in technology, and my years of professional experience give me a laser focus on stakeholder needs. 
+			I am a team focused Project Manager who reliably delivers results on time and on budget.  After 15 years in the live entertainment industry I am putting my management skills to use in the tech world. My course work at Harvard Extension has provided me with a strong foundation in technology and my years of professional experience give me a laser focus on stakeholder needs. 
 			</p> 
 			
 			<p>
-			I am passionate about pursuing my goals in my new field, and I never stop learning. I have a proven track record of facing new challanges, such as a degree in a whole new field, and quickly learning new skills. Check out my <a href="resume.php">resume</a> to read a bit more about my qualifications and background.
+			I am passionate about pursuing my goals in my new field and I never stop learning. I have a proven track record of facing new challanges, such as a degree in a whole new field, and quickly learning new skills. Check out my <a href="resume.php">resume</a> to read a bit more about my qualifications and background.
 			</p>
 			<div id="links" class="blur">
 				<span>Contact me at <a href="+19177419221">917.741.9221</a> or <a href="mailto:michael.pttrson@gmail.com">michael.pttrson@gmail.com</a>.  </span>
