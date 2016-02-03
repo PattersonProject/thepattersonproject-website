64564adeeea898e7a4b52197a316480f9fbd8c01
Michael Patterson <michael.pttrson@gmail.com>
2016-02-02 20:27:00 -0500

    New Background and copy

--- index.php
+++ index.php
@@ -16,11 +16,11 @@
 		<div id="content">
 			<img class="profile" src="images/profile-pic.png" alt="My profile picture">
 			<p>
-			I am a team focused Project Manager who reliably delivers results on time and on budget.  After 15 years in the live entertainment industry I am putting my management skills to use in the tech world. My course work at Harvard Extension has provided me with a strong foundation in technology and my years of professional experience give me a laser focus on stakeholder needs. 
+			I am a skilled team builder who gets results. As an experienced project manager I stay focused on the goals of stakeholders while tracking down the tiniest details that might threaten success. Now I am adding web design and development to my skill set by completing my MLA in Information Systems Management this spring at Harvard Extension. I look forward to combining years of experience marshaling resources for live theatrical productions with my newly acquired skills in front end development to create experiences that users want and stakeholders need. As a strategic thinker I can work both as a team member and a leader letting the big picture inform how I go about problem solving.
 			</p> 
 			
 			<p>
-			I am passionate about pursuing my goals in my new field and I never stop learning. I have a proven track record of facing new challenges, such as a degree in a whole new field, and quickly learning new skills. Check out my <a href="resume.php">resume</a> to read a bit more about my qualifications and background.
+			I am passionate about pursuing my goals in my new field and I never stop learning. I have a proven track record of facing new challenges and quickly learning new skills. Check out my  <a href="resume.php">resume</a> to read a bit more about my qualifications and background.
 			</p>
 			<div id="links" class="blur">
 				<span>Contact me at <a href="+19177419221">917.741.9221</a> or <a href="mailto:michael.pttrson@gmail.com">michael.pttrson@gmail.com</a>.  </span>
@@ -29,10 +29,7 @@
 			</div>
 		
 			<p>
-			I built my previous career in a variety of arts organizations.  These experiences taught me the importance of high quality project management and strategic planning.  A live event has a hard deadline, and my project management skills have led to many successful opening nights.  I am an expert at building strong teams, and keeping them focused on the larger project goals.
-			</p>
-			<p>
-			Contact me and let's discuss how we can make your next project a success! 
+			 If you have a dynamic team environment that needs cohesion and efficiency I can use my skills to create strong, innovative solutions. 
 			</p>
 			<!-- <div id="links" class="blur">
 				<span>Contact me at <a href="+19177419221">917.741.9221</a> or <a href="mailto:michael.pttrson@gmail.com">michael.pttrson@gmail.com</a>.  </span>
