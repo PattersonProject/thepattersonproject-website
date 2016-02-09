bf08caf3df380440c9d4791693741f75b3d6ce11
Michael Patterson <michael.pttrson@gmail.com>
2016-02-08 21:40:22 -0500

    Updated Intro Copy

--- index.php
+++ index.php
@@ -16,11 +16,11 @@
 		<div id="content">
 			<img class="profile" src="images/profile-pic.png" alt="My profile picture">
 			<p>
-			I am a skilled team builder who gets results. As an experienced project manager I stay focused on the goals of stakeholders while tracking down the tiniest details that might threaten success. Now I am adding web design and development to my skill set by completing my MLA in Information Systems Management this spring at Harvard Extension. I look forward to combining years of experience marshaling resources for live theatrical productions with my newly acquired skills in front end development to create experiences that users want and stakeholders need. As a strategic thinker I can work both as a team member and a leader letting the big picture inform how I go about problem solving.
+			I believe the success of any project lies in the strength of its team. As an experienced project manager I build teams that stay focused on the goals of stakeholders while allowing team members to track down the tiniest details that might threaten success. Now I am adding web design and development to my skill set by completing my MLA in Information Systems Management this spring at Harvard Extension. I look forward to combining years of experience marshaling resources for live theatrical productions with my newly acquired skills in front end development to create experiences that users want and stakeholders need. As a strategic thinker I can build and deploy a team that will tackle your most critical projects and deliver results, on time and on budget.
 			</p> 
 			
 			<p>
-			I am passionate about pursuing my goals in my new field and I never stop learning. I have a proven track record of facing new challenges and quickly learning new skills. Check out my  <a href="resume.php">resume</a> to read a bit more about my qualifications and background.
+			I am passionate about pursuing my goals in my new field and I never stop learning. I have a proven track record of facing new challenges and quickly learning new skills. I will come up to speed on your processes and culture quickly in order to integrate the goals of any project with the direction of the organization. Check out my  <a href="resume.php">resume</a> to read a bit more about my qualifications and background.
 			</p>
 			<div id="links" class="blur">
 				<span>Contact me at <a href="+19177419221">917.741.9221</a> or <a href="mailto:michael.pttrson@gmail.com">michael.pttrson@gmail.com</a>.  </span>
@@ -29,7 +29,7 @@
 			</div>
 		
 			<p>
-			 If you have a dynamic team environment that needs cohesion and efficiency I can use my skills to create strong, innovative solutions. 
+			My project management skills have led to many successful opening nights. If you have a dynamic team environment that needs cohesion and efficiency I can use my skills to create strong, innovative solutions.  
 			</p>
 			<!-- <div id="links" class="blur">
 				<span>Contact me at <a href="+19177419221">917.741.9221</a> or <a href="mailto:michael.pttrson@gmail.com">michael.pttrson@gmail.com</a>.  </span>
