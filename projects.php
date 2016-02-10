a3da26da417f76e4281766b1607039dfa4827654
Michael Patterson <michael.pttrson@gmail.com>
2016-02-09 20:32:50 -0500

    Proofreading edits

--- projects.php
+++ projects.php
@@ -17,7 +17,7 @@
 		<p>During my coursework I have developed a number of projects for my classes. Select a link below for more information on these individual projects.</p>
 		<h2>Soapworks</h2>
 		<p>This is a mockup of a customer facing site of a (fictional) artisanal soap company. This site includes several product pages, a custom order form, and product pop-ups.</p>
-		<p>Read about this project <a href="soapworks.php">here</a> and see the <a href="http://soapworks-mpatterson.rhcloud.com/">live deployment here</a>
+		<p>Read about this project <a href="soapworks.php">here</a> and see the <a href="http://soapworks-mpatterson.rhcloud.com/">live deployment here</a>.</p>
 		<h2>Utopia</h2>
 		<p>Utopia offers Boston Taxi cabs a technological solution to the heightened competition from Transportation Network Companies like Uber and Lyft. This project was developed as my Capstone Project for the degree program at Harvard Extension.</p>
 		<p>Learn more <a href="utopia.php">here</a>.</p>
--- soapworks.php
+++ soapworks.php
@@ -16,16 +16,16 @@
 		<div id="content">
 			<h1>Patterson Soapworks</h1>
 				<p>Live deployement: <a href="http://soapworks-mpatterson.rhcloud.com/">http://soapworks-mpatterson.rhcloud.com/</a><br />Github: <a href="https://github.com/PattersonProject/SoapWorks">https://github.com/PattersonProject/SoapWorks</a></p>
-				<p>This is a customer facing website for a fictional, artisanal soap company. The goal is to highlight certain products and easily allow customers to browse the options, or fill in a form for custom orders. The pages are designed to be responsive for viewing on a variety of screens. The input on any form (including the &lsquo;add to cart&rsquo; form) is simply echoed back by a script on the Harvard Morpheus server to test functionality. This project started as part of classwork for Fundamentals of Website Design at Harvard Extension, and has been expanded since the course ended.</p>
-				<p>Overall I was happy with the way this site turned out. &nbsp;I was, perhaps, a little over eager with background images and textures but I gained experience on fittine and editing backgrounds.&nbsp;</p>
+				<p>This is a customer facing website for a fictional, artisanal soap company. The goal is to highlight certain products and easily allow customers to browse the options, or fill in a form for custom orders. The pages are designed to be scaled for viewing on a variety of screens. The input on any form (including the &lsquo;add to cart&rsquo; form) is simply echoed back by a script on the Harvard Morpheus server to test functionality. This project started as part of classwork for Fundamentals of Website Design at Harvard Extension, and has been expanded since the course ended.</p>
+				<p>Overall I was happy with the way this site turned out. &nbsp;I was, perhaps, a little over eager with background images and textures but I gained experience on sizing and editing backgrounds.&nbsp; Using the PHP includes for product previews made me look forward to learning about content management and methods for dynamically updated a site like this.</p>
 				<h2>Features</h2>
 				<ul>
-				<li>Each of the product preview boxes will show an expanded view using the Fancybox JQuery plugin when the 'Quick Look' button is clicked</li>
+				<li>Each of the product preview boxes will show an expanded view using the Fancybox JQuery plugin when the 'Quick Look' button is clicked.</li>
 				<li>These product preview boxes are all dropped in using a PHP include which allows for them to be updated and changed quickly as needed.</li>
-				<li>The 'Add to Cart' buttons forward the quantity to an echo script on Harvard's Morpheus server which will return the variables sent</li>
-				<li>The 'Request a Quote' page utilizes the same script to echo the form data</li>
+				<li>The 'Add to Cart' buttons forward the quantity to an echo script on Harvard's Morpheus server which will return the variables sent.</li>
+				<li>The 'Request a Quote' page utilizes the same script to echo the form data.</li>
 				<li>The 'Request a Quote' form uses an accordion plugin from twitter Bootstrap using Javascript. If Javascript is disabled the form will display fully.</li>
-				<li>'Contact Us' and 'Your account' are not implemented at this time</li>
+				<li>'Contact Us' and 'Your account' are not implemented at this time.</li>
 				</ul>
 				<h2>ToDo</h2>
 				<ul>
