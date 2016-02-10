3c5e14adce63e3ef7fdab1e04672ff4561e671a8
Michael Patterson <michael.pttrson@gmail.com>
2016-02-09 21:15:40 -0500

    Updated footer CSS

--- resume.php
+++ resume.php
@@ -19,9 +19,90 @@
 			<a href="content/Michael.Patterson.Resume.pdf">Download  <img src="images/download-wht.png" alt="Download"> </a>
 			</span>
 
-			<object type="application/pdf" data="content/Michael.Patterson.Resume.pdf">
-				<p> It looks like the pdf failed to load!  Download my resume <a href="content/Michael.Patterson.Resume.pdf">here</a> ! </p>
-			</object>
+			<h1>&shy;&shy;&shy;&shy;<br />Michael J. Patterson</h1>
+<h3>Summary of Qualifications:</h3>
+<ul>
+<li>Solid foundation in Project Management including familiarity with PMI&rsquo;s PMBOK and project lifecycle management
+<ul>
+<li>Familiar with Waterfall, Agile (including Scrum), Lean, methodologies</li>
+<li>
+<p>Assumed the role of Project Manager in my team for our Capstone Project, the culminating project of the&nbsp;Information Systems Management Degree Program at Harvard Extension</p>
+</li>
+<li>
+<p>Recently acted as Project Manager for a management class team project proposing an e-commerce website development for a small music production label</p>
+</li>
+</ul>
+</li>
+<li>Managed a team for the Capstone Project which proposed an integrated Enterprise Taxi Dispatch System that would allow Boston&rsquo;s public Taxi&rsquo;s to compete with Uber and Lyft
+<ul>
+<li>The project included a well defined business case and realistic, measurable success metrics</li>
+<li>Components included Amazon EC2 instances, Amazon RDS, Cabforce backend and iDashboard</li>
+<li>A complete implementation plan was developed including a pilot program to ensure widespread user adoption, and integration into existing systems</li>
+</ul>
+</li>
+<li>Proven track record of translating stakeholder needs into actionable, achievable team goals</li>
+<li>Specializes in building effective team as a results oriented manager</li>
+<li>Proven enthusiastic problem solver who thrives in fast paced environments</li>
+</ul>
+<h3>Related Experience:</h3>
+<h4 style="padding-left: 30px;">Blue Man Group, Boston, MA 2009 &ndash; 2015</h4>
+<h5 style="padding-left: 30px;">Project Manager</h5>
+<ul>
+<li>
+<p>Managed scope and executed projects on time and on budget meeting the needs of multiple stakeholders</p>
+</li>
+<li>Reduced department budget by more than 20% over 6 years and controlled costs throughout my tenure</li>
+<li>Managed cross functional team that interfaced with multiple other departments</li>
+<li>Coordinated major renovation project for both Blue Man management and building management</li>
+<li>Oversaw the budgeting and staffing of several long term projects, including new artistic content installation</li>
+<li>Integrated new automated scenery protocols into the existing show run structure</li>
+<li>Installed new video technology for Lobby video panels</li>
+<li>Developed detailed inspection and maintenance schedules across departments</li>
+<li>Streamlined training procedures for multiple departments</li>
+</ul>
+<h4>Boston Lyric Opera, Boston, MA 2004 &ndash; 2008</h4>
+<h5><br />Production Manager (2006 &ndash; 2008)</h5>
+<ul>
+<li>Improved accuracy of $3 million yearly production budget using top-down and bottom-up tactics</li>
+<li>Ensured high quality technical execution of four productions each year on a rigorous production schedule</li>
+<li>Established key financial metrics for monitoring progress on several concurrent productions</li>
+<li>Used &lsquo;Balanced Score Card&rsquo; metrics to aid in long term planning</li>
+<li>Managed 5 person full time team and dozens of contract team members</li>
+<li>Negotiated contracts with Designers, co-producing companies, vendors and sub-contractors</li>
+</ul>
+<h5>Production Coordinator / Production Administrator (2004 &ndash; 2006)</h5>
+<ul>
+<li>Independently managed educational production expenses and revenue</li>
+<li>Reduced costs by establishing new administration procedures and revising production schedules</li>
+<li>Created and expanded new revenue opportunities and expense tracking procedures</li>
+<li>Streamlined payroll administration for union stagehands, non-union employees and teamsters</li>
+</ul>
+<h5>Safe Horizon Events, New York, NY 2000 &ndash; 2004<br />Assistant Production Manager</h5>
+<ul>
+<li>Assisted in development of budgets, timelines and schedules</li>
+<li>Estimated labor, equipment and administrative costs</li>
+<li>Assisted in coordination of technical aspects of events in a variety of non-traditional venues</li>
+</ul>
+<h3>Other Experience:</h3>
+<h4>Freelance Lighting Designer and Technician</h4>
+<ul>
+<li>Assistant Projection Designer, Wicked The Gershwin Theatre, Broadway 2003</li>
+<li>Assistant Lighting Designer, Blast! 1st National U.S. tour, Japan Tour 2000, 2001</li>
+<li>Sub Flyloft, Sub Electrician, Sub Deck 3, Blue Man Group, Boston, MA 1998 &ndash; 1999</li>
+<li>Lighting Supervisor, Boston Lyric Opera, Shubert Theatre, Boston, MA 1998 &ndash; 2004</li>
+</ul>
+<h4>Arlington Public Schools, Arlington, MA 1993 &ndash; 1996</h4>
+<h5>Middle School General Science Teacher</h5>
+<ul>
+<li>Taught two 7th and two 8th grade general science classes in accordance with approved curriculum</li>
+<li>Developed and implemented a cooperative learning structure meeting a wide range of learning styles</li>
+<li>Worked closely with Special Education liaisons to develop individual education plans for students</li>
+</ul>
+<h3>Education:</h3>
+<p><strong>Harvard University Extension School</strong>, Cambridge, MA 2016<br /><strong>Master of Liberal Arts</strong> in progress expected completion Spring 2016. Current GPA 3.97<br />Concentration: Information Systems Management</p>
+<p>Classwork included:<br />&middot; Solid training in Java, PHP, Laravel framework ,HTML, CSS, Javascript, JQuery and SQL<br />&middot; Skill development using Git, SaaS and PaaS for a variety of class projects<br />&middot; Introductions to Python, MongoDB, XQuery and Berkley DB<br />&middot; Exposure in current practices in network architecture and security protocols</p>
+<p><strong>Emerson College</strong>, Boston, MA 1998<br /><strong>Master of Arts</strong>, Concentration: Theatre Education</p>
+<p><strong>Brandeis University</strong>, Waltham, MA 1992<br /><strong>Bachelor of Arts</strong>, Concentration: Physics</p>
 		</div>
 	</body>
 </html>
---
+++ resume2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title> Michael Patterson Project Manager </title>
+		<link rel="stylesheet" type="text/css" href="css/resume.css">
+		<?php include("include/head.php");?> 
+	</head>
+
+
+	<body id="resume" class="whoo">
+
+
+		<?php include ("include/header.php");?>
+
+		<div id="content">
+
+			<span>	
+			<a href="content/Michael.Patterson.Resume.pdf">Download  <img src="images/download-wht.png" alt="Download"> </a>
+			</span>
+
+			<object type="application/pdf" data="content/Michael.Patterson.Resume.pdf">
+				<p> It looks like the pdf failed to load!  Download my resume <a href="content/Michael.Patterson.Resume.pdf">here</a> ! </p>
+			</object>
+		</div>
+	</body>
+</html>
